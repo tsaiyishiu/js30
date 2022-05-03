d83f45c973d3f35d59246fb12d420f1b377dc343
yishiu <zxc86655050zxc@gmail.com>
2022-05-03 12:15:16 +0800

    js13

---
+++ jsday13/js13.js
@@ -0,0 +1,53 @@
+
+let images = document.querySelectorAll('img')
+function debounce(func, wait = 20, immediate = true) {
+    
+    // var = undefined
+    var timeout;
+    
+    return function() {
+      var context = this, args = arguments;
+    
+      // console.log(arguments)
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+        console.log(func.apply(context, args))
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
+  
+
+function scrollhandler (){
+    //視窗的最高
+    let windowTop = window.scrollY;
+    //視窗的對底
+    let windowBottom = windowTop + window.innerHeight;
+    images.forEach((img)=>{
+        //                圖片的top ＋ 圖片的一半高度
+        let imgmidle = img.offsetTop + img.height/2
+        //如果圖片位置 < 視窗對底部和圖片位置過半 > 視窗最高    
+        if(imgmidle < windowBottom && imgmidle > windowTop){
+            //執行active動作
+            img.classList.add('active')
+        } else{
+            //不然移除active動作
+            img.classList.remove('active');
+        }
+    });
+
+    console.log('scroll', new Date().getTime())
+}
+window.addEventListener('scroll',debounce(scrollhandler));
+
+
+
+// function debounce(func, wait = 20, immediate = true)
+
+//          debounce(scrollhandler,  ,   )
+
+
