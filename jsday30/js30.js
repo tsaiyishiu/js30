696443fa8eda657f2969d773f67e28ba8f53e210
yishiu <zxc86655050zxc@gmail.com>
2022-05-15 14:17:09 +0800

    js30

--- jsday30/js30.js
+++ jsday30/js30.js
@@ -1,55 +1,60 @@
-const scoreBoard = document.querySelector('.score');
-const moles = [...document.querySelectorAll('.mole')];
-const status = moles.reduce((prev,current,index)=>{
+const scoreBoard = document.querySelector(".score");
+const moles = [...document.querySelectorAll(".mole")];
+const status = moles.reduce((prev, current, index) => {
   prev[index] = false;
   return prev;
-},{})
+}, {});
 //console.log(status)
 
-const clickhandler = function(){
-  if(molesProxy[moles.indexOf(this)]){
+const clickhandler = function () {
+  if (molesProxy[moles.indexOf(this)]) {
     setscore(score + 1);
     molesProxy[moles.indexOf(this)] = false;
   }
 };
-const molesProxy =new Proxy(status,{
-  get(target, key){
+const molesProxy = new Proxy(status, {
+  get(target, key) {
     return target[key];
   },
-  set(target, key, value){
+  set(target, key, value) {
     target[key] = value;
-    moles[key].removeEventListener('click', clickhandler);
-    if(value){
-      moles[key].addEventListener('click', clickhandler) 
-      moles[key].classList.add('up');
-    }else{
-      
-      moles[key].classList.remove('up'); 
+    moles[key].removeEventListener("click", clickhandler);
+    if (value) {
+      moles[key].addEventListener("click", clickhandler);
+      moles[key].classList.add("up");
+    } else {
+      moles[key].classList.remove("up");
     }
-  }
-})
+  },
+});
 
 let score = 0;
 let timeUp = true;
 
-const setscore = function(s){
+const setscore = function (s) {
   score = s;
   scoreBoard.textContent = score;
 };
 
-const getRandomMole = function(){
+const getRandomMole = function () {
+  const mole = Math.floor(Math.random() * moles.length);
+  const time = Math.random() * (1000 - 500) + 500; // 500~1000
+  if (molesProxy[mole]) return getRandomMole();
+  molesProxy[mole] = true;
+  setTimeout(() => {
+    if (!timeUp) getRandomMole();
+    molesProxy[mole] = false;
+  }, time);
+};
 
-}
-
-const startGame = function(){
-  setscore(0)
+const startGame = function () {
+  setscore(0);
   timeUp = false;
   getRandomMole();
   setTimeout(() => {
-    (timeUp = true), alert('loser') //10秒後彈出對話匡
+    (timeUp = true), alert("loser"); //10秒後彈出對話匡
   }, 10000);
-}
-
+};
 
 //---------------------------------proxy測試---------------------------------
 // let obj = {
@@ -67,19 +72,6 @@ const startGame = function(){
 //     target[ket] = value;
 //   }
 // })
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // const holes = document.querySelectorAll('.hole');
 //   const scoreBoard = document.querySelector('.score');
