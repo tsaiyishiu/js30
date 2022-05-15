6a609398eba1e84d4792b05a29e3552764c918ab
yishiu <zxc86655050zxc@gmail.com>
2022-05-15 13:44:46 +0800

    js30 helf

---
+++ jsday30/js30.js
@@ -0,0 +1,131 @@
+const scoreBoard = document.querySelector('.score');
+const moles = [...document.querySelectorAll('.mole')];
+const status = moles.reduce((prev,current,index)=>{
+  prev[index] = false;
+  return prev;
+},{})
+//console.log(status)
+
+const clickhandler = function(){
+  if(molesProxy[moles.indexOf(this)]){
+    setscore(score + 1);
+    molesProxy[moles.indexOf(this)] = false;
+  }
+};
+const molesProxy =new Proxy(status,{
+  get(target, key){
+    return target[key];
+  },
+  set(target, key, value){
+    target[key] = value;
+    moles[key].removeEventListener('click', clickhandler);
+    if(value){
+      moles[key].addEventListener('click', clickhandler) 
+      moles[key].classList.add('up');
+    }else{
+      
+      moles[key].classList.remove('up'); 
+    }
+  }
+})
+
+let score = 0;
+let timeUp = true;
+
+const setscore = function(s){
+  score = s;
+  scoreBoard.textContent = score;
+};
+
+const getRandomMole = function(){
+
+}
+
+const startGame = function(){
+  setscore(0)
+  timeUp = false;
+  getRandomMole();
+  setTimeout(() => {
+    (timeUp = true), alert('loser') //10秒後彈出對話匡
+  }, 10000);
+}
+
+
+//---------------------------------proxy測試---------------------------------
+// let obj = {
+//   a: true,
+//   b: true
+// };
+
+// let objproxy = new Proxy(obj, {
+//   get(target, key){
+//     console.log(target, key);
+//     return target[key];
+//   },
+//   set(target, key, value){
+//     console.log(key, value);
+//     target[ket] = value;
+//   }
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const holes = document.querySelectorAll('.hole');
+//   const scoreBoard = document.querySelector('.score');
+//   const moles = document.querySelectorAll('.mole');
+//   let lastHole;
+//   let timeUp = false;
+//   let score = 0;
+
+//   function randomTime(min, max) {
+//     return Math.round(Math.random() * (max - min) + min);
+//   }
+
+//   function randomHole(holes) {
+//     const idx = Math.floor(Math.random() * holes.length);
+//     const hole = holes[idx];
+//     if (hole === lastHole) {
+//       console.log('Ah nah thats the same one bud');
+//       return randomHole(holes);
+//     }
+//     lastHole = hole;
+//     return hole;
+//   }
+
+//   function peep() {
+//     const time = randomTime(200, 1000);
+//     const hole = randomHole(holes);
+//     hole.classList.add('up');
+//     setTimeout(() => {
+//       hole.classList.remove('up');
+//       if (!timeUp) peep();
+//     }, time);
+//   }
+
+//   function startGame() {
+//     scoreBoard.textContent = 0;
+//     timeUp = false;
+//     score = 0;
+//     peep();
+//     setTimeout(() => timeUp = true, 10000)
+//   }
+
+//   function bonk(e) {
+//     if(!e.isTrusted) return; // cheater!
+//     score++;
+//     this.parentNode.classList.remove('up');
+//     scoreBoard.textContent = score;
+//   }
+
+//   moles.forEach(mole => mole.addEventListener('click', bonk));
